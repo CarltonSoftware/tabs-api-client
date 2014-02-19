2800fe160db0b8bfae8fc3a2a985e91ab934d22a
Alex Wyett <alex@carltonsoftware.co.uk>
2014-02-19 16:42:20 +0000

    Fixed checkstyle errors

--- autoload.php
+++ autoload.php
@@ -13,31 +13,33 @@
  * @link      http://www.carltonsoftware.co.uk
  */
 
-spl_autoload_register(function($class) {
-    
-    // Project specific namespace
-    $prefix = 'tabs\\api\\';
-    
-    // Base directory
-    $base_dir = __DIR__ . '/src/';
-    
-    // Does the class use this namespace?
-    $len = strlen($prefix);
-    
-    // @codeCoverageIgnoreStart
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no
-        return;
+spl_autoload_register(
+    function ($class) {
+
+        // Project specific namespace
+        $prefix = 'tabs\\api\\';
+
+        // Base directory
+        $base_dir = __DIR__ . '/src/';
+
+        // Does the class use this namespace?
+        $len = strlen($prefix);
+
+        // @codeCoverageIgnoreStart
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no
+            return;
+        }
+        // @codeCoverageIgnoreEnd
+
+        // Replace namespace prefix with the base directory, replace namepace
+        // separators with directory separators in teh relative class name,
+        // append with .class.php
+        $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+
+        if (file_exists($file)) {
+            include $file;
+        }
+
     }
-    // @codeCoverageIgnoreEnd
-    
-    // Replace namespace prefix with the base directory, replace namepace
-    // separators with directory separators in teh relative class name,
-    // append with .class.php
-    $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-    
-    if (file_exists($file)) {
-        require $file;
-    }
-    
-});
+);
