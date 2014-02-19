940dd7556658e2ca1b6c492e1697af17534bd9a9
Alex Wyett <alex@carltonsoftware.co.uk>
2014-02-19 16:08:33 +0000

    Updated and moved files into correct folder structure for psr-0 format

--- tabs/autoload.php
+++ tabs/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Tabs API Client autoloader.
+ * Legacy Tabs API Client autoloader.
  *
  * PHP Version 5.3
  *
@@ -12,18 +12,4 @@
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link      http://www.carltonsoftware.co.uk
  */
-
-namespace tabs;
-define('TABSAPIDS', DIRECTORY_SEPARATOR);
-
-spl_autoload_extensions(".class.php");
-
-spl_autoload_register(
-    function ($pClassName) {
-        $file = str_replace('\\', TABSAPIDS, $pClassName) . '.class.php';
-        $file = dirname(__FILE__) . TABSAPIDS . '..' . TABSAPIDS . $file;
-        if (file_exists($file)) {
-            include $file;
-        }
-    }
-);
+require_once '../autoload.php';
