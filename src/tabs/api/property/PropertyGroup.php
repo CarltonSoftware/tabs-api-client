6b5990f5e2e12663616633401a12a34f656aec45
Alex Wyett <alex.wyett@gmail.com>
2014-09-18 11:50:21 +0100

    Added more accessors to PropertyGroup.

--- src/tabs/api/property/PropertyGroup.php
+++ src/tabs/api/property/PropertyGroup.php
@@ -76,6 +76,174 @@ class PropertyGroup extends \tabs\api\core\Base
         
         return $this;
     }
+    
+    /**
+     * Remove a property from the array
+     * 
+     * @param string $propId Property id
+     * 
+     * @return \tabs\api\property\PropertyGroup
+     */
+    public function removeProperty($propId)
+    {
+        if (isset($this->properties[$propId])) {
+            unset($this->properties[$propId]);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Return the number of properties in the property group.
+     * 
+     * @return integer
+     */
+    public function getPropertyCount()
+    {
+        return count($this->getProperties());
+    }
+    
+    /**
+     * Return the changeover day of the property group
+     * 
+     * @return string
+     */
+    public function getChangeDay()
+    {
+        $properties = $this->getProperties();
+        $chd = 'Saturday';
+        if (count($properties) > 0) {
+            $first = array_shift($properties);
+            $chd = $first->getChangeOverDay();
+        }
+        
+        return $chd;
+    }
+    
+    /**
+     * Return true if all of the properties in the group accept pets or not
+     * 
+     * @return boolean
+     */
+    public function hasPets()
+    {
+        $pets = true;
+        foreach ($this->getProperties() as $property) {
+            if ($property->hasPets() === false) {
+                $pets = false;
+            }
+        }
+        
+        return $pets;
+    }
+    
+    /**
+     * Return the maximum accommodation for the property group
+     * 
+     * @return integer
+     */
+    public function getMaximumAccommodation()
+    {
+        return $this->_getMaximumValue('getAccommodates');
+    }
+    
+    /**
+     * Return the minimum accommodation for the property group
+     * 
+     * @return integer
+     */
+    public function getMinimumAccommodation()
+    {
+        return $this->_getMinimumValue('getAccommodates');
+    }
+    
+    /**
+     * Return the maximum bedrooms for the property group
+     * 
+     * @return integer
+     */
+    public function getMaximumBedrooms()
+    {
+        return $this->_getMaximumValue('getBedrooms');
+    }
+    
+    /**
+     * Return the minimum bedrooms for the property group
+     * 
+     * @return integer
+     */
+    public function getMinimumBedrooms()
+    {
+        return $this->_getMinimumValue('getBedrooms');
+    }
+    
+    /**
+     * Return the maximum value of the groups properties using a give property
+     * object accessor
+     * 
+     * @param string $accessor Property accessor to call
+     * 
+     * @return string
+     */
+    private function _getMaximumValue($accessor)
+    {
+        $value = 0;
+        foreach ($this->getProperties() as $property) {
+            if ($value === 0) {
+                $value = $property->$accessor();
+            } else {
+                $value += $property->$accessor();
+            }
+        }
+        
+        return $value;
+    }
+    
+    /**
+     * Return the minimum value of the groups properties using a give property
+     * object accessor
+     * 
+     * @param string $accessor Property accessor to call
+     * 
+     * @return string
+     */
+    private function _getMinimumValue($accessor)
+    {
+        $value = null;
+        foreach ($this->getProperties() as $property) {
+            if ($value === null) {
+                $value = $property->$accessor();
+            } else if ($value > $property->$accessor()) {
+                $value = $property->getAccommodates();
+            }
+        }
+        
+        return ($value) ? $value : 0;
+    }
+    
+    /**
+     * Return a collection of calendars
+     * 
+     * @param integer $start   Timestamp of the start month
+     * @param integer $end     Timestamp of the end month
+     * @param array   $options Calendar options array
+     * 
+     * @return array
+     */
+    public function getCalendars($start, $end, $options = array())
+    {
+        // Make sure start is start of month
+        $start = strtotime('first day of this month 00:00:00', $start);
+        
+        $calendars = array();
+        while ($end > $start) {
+            array_push($calendars, $this->getCalendar($start, $options));
+            
+            $start = strtotime('+ 1 month', $start);
+        }
+        
+        return $calendars;
+    }
 
     /**
      * Output a months availability with the day number as the index of the
@@ -91,7 +259,7 @@ class PropertyGroup extends \tabs\api\core\Base
         $targetMonth = null,
         $options = array()
     ) {
-        $monthArray = $this->_getAvailability($targetMonth);
+        $monthArray = $this->getAvailability($targetMonth);
         $calendar = '';
         if (count($monthArray) > 0) {
             $calObj = new \tabs\api\property\Calendar($options);
@@ -110,7 +278,7 @@ class PropertyGroup extends \tabs\api\core\Base
      * 
      * @return array
      */
-    private function _getAvailability($month)
+    public function getAvailability($month)
     {
         $availability = array();
         foreach ($this->getProperties() as $property) {
